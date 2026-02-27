df4b3173ea8396e8c5130d5daa00b44332f910bb
mxgic1337 <60188749+mxgic1337@users.noreply.github.com>
2026-02-27 18:37:45 +0100

    feat: change the message returned by `/live`

--- routes/live.ts
+++ routes/live.ts
@@ -3,5 +3,5 @@ import express from 'express';
 export const liveRoute = express.Router();
 
 liveRoute.get('/:playerName', (_, res) => {
-  res.send("Funkcja została wycofana ze względu na ostatnie zmiany na FACEIT.")
+  res.send("Funkcja została wycofana ze względu na ostatnie zmiany w API FACEIT'a.")
 });
